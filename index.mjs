5928aa3d2f46a478d58e82c526b33c256ae245fe
xRuiAlves <ruialves.esrt.98@gmail.com>
2023-02-25 10:15:55 +0000

    Add file ID number validation

--- index.mjs
+++ index.mjs
@@ -28,5 +28,9 @@ export const handler = async({operation, fideId}) => {
     if (!OPERATIONS[operation]) {
         return buildErrorResponse(`You must specify a valid 'operation' within the request's body. Valid operations: [${Object.values(OPERATIONS).join(", ")}].`); 
     }
+
+    if (!fideId || isNaN(fideId)) {
+        return buildErrorResponse("You must specify a valid 'fideId' number within the request's body."); 
+    }
     return buildSuccessMessage("Hello from Lambda");
 };

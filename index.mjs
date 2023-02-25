e49f6b337fa37cba55676db68a492b5039ae2f28
xRuiAlves <ruialves.esrt.98@gmail.com>
2023-02-25 10:15:06 +0000

    Add utility functions to build lambda responses

--- index.mjs
+++ index.mjs
@@ -1,8 +1,20 @@
-export const handler = async(event) => {
-    console.info(event);
-    const response = {
-        statusCode: 200,
-        body: JSON.stringify("Hello from Lambda!"),
-    };
-    return response;
+import { 
+    getPlayerElo, 
+    getPlayerHistory, 
+    getPlayerRank, 
+    getPlayerFullInfo, 
+    getPlayerPersonalData 
+} from "fide-ratings-scraper";
+
+const buildErrorResponse = (message) => ({
+    statusCode: 400,
+    body: message,
+});
+
+const buildSuccessMessage = (payload) => ({
+    statusCode: 200,
+    body: JSON.stringify(payload)
+});
+
+export const handler = async({operation, fideId}) => {
 };

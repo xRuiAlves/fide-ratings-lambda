5e10cf142d65ae5d6a3d96c43a783980c72386a6
xRuiAlves <ruialves.esrt.98@gmail.com>
2023-02-25 10:49:01 +0000

    Improve logging

--- index.mjs
+++ index.mjs
@@ -24,23 +24,46 @@ const buildSuccessMessage = (payload) => ({
     body: JSON.stringify(payload)
 });
 
-export const handler = async({operation, fideId}) => {
+export const handler = async(event) => {
+    console.info(`Event body: ${event.body}`);
+    
+    const { operation, fideId } = JSON.parse(event.body);
+    console.info(`Operation: ${operation}`);
+    console.info(`Operation: ${fideId}`);
+
     if (!fideId || isNaN(fideId)) {
+        console.error("Invalid fide ID");
         return buildErrorResponse("You must specify a valid 'fideId' number within the request's body."); 
     }
 
     switch (operation) {
         case OPERATIONS.getPlayerElo:
-            return buildSuccessMessage(await getPlayerElo(fideId));
+            console.info("Getting player elo");
+            const elo = await getPlayerElo(fideId);
+            console.info(`Player ${fideId} elo: ${elo}`);
+            return buildSuccessMessage(elo);
         case OPERATIONS.getPlayerHistory:
-            return buildSuccessMessage(await getPlayerHistory(fideId));
+            console.info("Getting player history");
+            const history = await getPlayerHistory(fideId);
+            console.info(`Player ${fideId} history: ${history}`);
+            return buildSuccessMessage(history);
         case OPERATIONS.getPlayerRank:
-            return buildSuccessMessage(await getPlayerRank(fideId));
+            console.info("Getting player rank");
+            const rank = await getPlayerRank(fideId);
+            console.info(`Player ${fideId} rank: ${rank}`);
+            return buildSuccessMessage(rank);
         case OPERATIONS.getPlayerFullInfo:
-                return buildSuccessMessage(await getPlayerFullInfo(fideId));
+            console.info("Getting player full info");
+            const fullInfo = await getPlayerFullInfo(fideId);
+            console.info(`Player ${fideId} full info: ${fullInfo}`);
+            return buildSuccessMessage(fullInfo);
         case OPERATIONS.getPlayerPersonalData:
-            return buildSuccessMessage(await getPlayerPersonalData(fideId));
+            console.info("Getting player personal data");
+            const personalData = await getPlayerPersonalData(fideId);
+            console.info(`Player ${fideId} personal data: ${personalData}`);
+            return buildSuccessMessage(personalData);
         default:
+            console.error("Invalida operation");
             return buildErrorResponse(
                 `You must specify a valid 'operation' within the request's body. Valid operations: [${Object.values(OPERATIONS).join(", ")}].`
             ); 

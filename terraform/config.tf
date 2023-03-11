c2bdbe4fc0dc19f02891dc5857aa35baf81e8582
xRuiAlves <ruialves.esrt.98@gmail.com>
2023-03-11 10:33:00 +0000

    Update terraform configs with fide ratings lambda function

--- terraform/config.tf
+++ terraform/config.tf
@@ -5,12 +5,16 @@ provider "aws" {
 resource "aws_lambda_function" "fide-ratings" {
   filename      = "fide-ratings.zip"
   function_name = "fide-ratings"
-  role          = aws_iam_role.lambda.arn
+  role          = aws_iam_role.fide-ratings.arn
   handler       = "index.handler"
-  runtime       = "nodejs14.x"
+  runtime       = "nodejs18.x"
+  timeout       = 60 # 1 minute
+  architectures = [
+    "arm64"
+  ]
 }
 
-resource "aws_iam_role" "fide-ratings-lambda-role" {
+resource "aws_iam_role" "fide-ratings" {
   name = "fide-ratings-lambda-role"
 
   assume_role_policy = jsonencode({
@@ -27,7 +31,7 @@ resource "aws_iam_role" "fide-ratings-lambda-role" {
   })
 }
 
-resource "aws_iam_role_policy_attachment" "lambda-execution-policy" {
+resource "aws_iam_role_policy_attachment" "fide-ratings-lambda-execution" {
   policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"
-  role       = aws_iam_role.lambda.name
+  role       = aws_iam_role.fide-ratings.name
 }
